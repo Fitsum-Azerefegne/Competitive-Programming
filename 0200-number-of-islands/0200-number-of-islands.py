c38cb327b1d765e6cbbe2e2d53493119b78615b5
Fitsum Azerefegne <mininifinds@gmail.com>
2025-08-11 17:10:56 +0300

    Time: 263 ms (37.08%), Space: 20.1 MB (82.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,19 +6,17 @@ class Solution:
         dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         def in_bound(r,c):
             return 0<=r< len(grid) and 0<=c<len(grid[0])
+        def dfs(i,j):
+            grid[i][j] = "0"  
+            for dx, dy in dir:
+                nx, ny = i + dx, j + dy
+                if in_bound(nx,ny) and grid[nx][ny] == "1":
+                    dfs(nx,ny)
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     islands += 1
-                    queue = deque()
-                    queue.append((i, j))
-                    grid[i][j] = "0"  
-                    while queue:
-                        x, y = queue.popleft()
-                        for dx, dy in dir:
-                            nx, ny = x + dx, y + dy
-                            if in_bound(nx,ny) and grid[nx][ny] == "1":
-                                queue.append((nx, ny))
-                                grid[nx][ny] = "0"
+                    dfs(i,j)
         return islands
                 
