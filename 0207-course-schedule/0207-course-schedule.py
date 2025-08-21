60f59b955059527f883abce8fd8a83209f4c399b
Fitsum Azerefegne <mininifinds@gmail.com>
2025-08-21 21:43:20 +0300

    Time: 7 ms (46.33%), Space: 18.8 MB (93.27%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
         graph = defaultdict(list)
         in_degree = defaultdict(int)
-
+        
         for u, v in prerequisites:
             graph[v].append(u)
             in_degree[u] += 1
             in_degree[v]
 
-        queue = deque([node for node in in_degree if in_degree[node] == 0])
-        sorted_order = []
-
+        queue = deque([v for v in range(numCourses) if in_degree[v] == 0])
+        taken_courses = 0
         while queue:
-            node = queue.popleft()
-            sorted_order.append(node)
-
-            for neighbor in graph[node]:
-                in_degree[neighbor] -= 1
-                if in_degree[neighbor] == 0:
-                    queue.append(neighbor)
-
-        if len(sorted_order) != len(in_degree):
-            return False
-        return True
-        
+            current_course = queue.popleft()
+            taken_courses += 1  
             
+            for next_course in graph[current_course]:
+                in_degree[next_course] -= 1
+                if in_degree[next_course] == 0:
+                    queue.append(next_course)
+
+        return taken_courses == numCourses
