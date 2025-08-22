b7b6cc9edb3ea5213ac4c7a4ea19a5c17514b22d
Fitsum Azerefegne <mininifinds@gmail.com>
2025-08-22 20:20:32 +0300

    Time: 5 ms (36.25%), Space: 19.1 MB (60.45%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -2,27 +2,22 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
         in_degree = defaultdict(int)
-
+        courses = []
         for u, v in prerequisites:
             graph[v].append(u)
             in_degree[u] += 1
             in_degree[v]
-        for i in range(numCourses):
-            if i not in in_degree:
-                in_degree[i] = 0
-        
-        queue = deque([node for node in in_degree if in_degree[node] == 0])
-        sorted_order = []
-        while queue:
-            node = queue.popleft()
-            sorted_order.append(node)
-            for neighbour in graph[node]:
-                in_degree[neighbour] -= 1
-                if in_degree[neighbour] == 0:
-                    queue.append(neighbour)
-        if len(sorted_order) != len(in_degree):
-            return []
-        return sorted_order
-        
 
+        queue = deque([v for v in range(numCourses) if in_degree[v] == 0])
+        while queue:
+            current_course = queue.popleft()
+            courses.append(current_course) 
+            
+            for next_course in graph[current_course]:
+                in_degree[next_course] -= 1
+                if in_degree[next_course] == 0:
+                    queue.append(next_course)
+        if len(courses) != numCourses:
+            return []
+        return courses
         
