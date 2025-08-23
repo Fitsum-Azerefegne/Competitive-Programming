36dd058422ec4981002f85ae25c2df3451d818c4
Fitsum Azerefegne <mininifinds@gmail.com>
2025-08-23 23:35:38 +0300

    Time: 114 ms (8%), Space: 24.2 MB (17.02%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,23 +1,21 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        reverse_graph = defaultdict(list)
         in_degree = defaultdict(int)
-        for u in range(n):
-            for v in graph[u]:
-                reverse_graph[v].append(u) # we reverse it so we can have terminal ones with no in degree
-                in_degree[u] += 1
-                in_degree[v]
-        queue = deque()
-        for i in range(n):
-            if in_degree[i] == 0:
-                queue.append(i)   
-        safe_states = []
+        reverse_graph = defaultdict(list)
+
+        for i in range(len(graph)):
+            for node in graph[i]:
+                reverse_graph[node].append(i)
+                in_degree[i] += 1
+                in_degree[node]
+        queue = deque([i for i in range(len(graph)) if in_degree[i] == 0])
+        safe_nodes = []
+
         while queue:
-            node = queue.popleft()
-            safe_states.append(node)
-            for neigh in reverse_graph[node]:
-                in_degree[neigh] -= 1
-                if in_degree[neigh] == 0:
-                    queue.append(neigh)
-        return sorted(safe_states)
+            curr_node = queue.popleft()
+            safe_nodes.append(curr_node)
+            for neighbour in reverse_graph[curr_node]:
+                in_degree[neighbour] -= 1
+                if in_degree[neighbour] == 0:
+                    queue.append(neighbour)
+        return sorted(safe_nodes)
