892b77de9f78481dce162170dfad9ef4b0baeb91
Fitsum Azerefegne <mininifinds@gmail.com>
2025-11-11 22:29:34 +0300

    Time: 0 ms (100%), Space: 17.8 MB (71.23%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         res = 0
-        for ppl in range (1, n+1):
-            res = (res+k) % ppl
-        return res + 1     
+        for ppl in range(1,n+1):
+           res = (res + k) % ppl 
+        return res + 1
