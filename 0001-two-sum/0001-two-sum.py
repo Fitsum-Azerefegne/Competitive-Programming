685932ebb0896ca944a95103f6906da835912e35
Fitsum Azerefegne <mininifinds@gmail.com>
2025-11-15 21:20:40 +0300

    Time: 0 ms (100%), Space: 18.9 MB (51.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
-        for index, num in enumerate(nums):
-            complement = target - num   
-            
+        for index,num in enumerate(nums):
+            complement = target - num
             if complement in hash_map:
-                return [hash_map[complement], index]
-            hash_map[num] = index
-        
-        return []
+                return [ hash_map[complement], index ]
+            else:
+                hash_map[num] = index
