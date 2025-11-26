884b3fa90b7557ba1bc3ebcde39cf1762a093f71
Fitsum Azerefegne <mininifinds@gmail.com>
2025-11-26 21:57:11 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -3,10 +3,7 @@ class Solution:
         g.sort()
         s.sort()
         happy_children = 0
-        g_pointer = s_pointer = 0
-        while (g_pointer < len(g)) and (s_pointer < len(s)):
-            if s[s_pointer] >= g[g_pointer]:
+        for i in range(min(len(s),len(g))):
+            if g[i] <= s[i]:
                 happy_children += 1
-                g_pointer += 1
-            s_pointer +=1
         return happy_children
