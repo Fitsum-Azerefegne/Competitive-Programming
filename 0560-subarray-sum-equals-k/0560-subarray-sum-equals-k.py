a500aabe18a026e8ba2171edeb3a7d214571d9d2
Fitsum Azerefegne <mininifinds@gmail.com>
2025-12-01 11:26:24 +0300

    Time: 31 ms (61.33%), Space: 20.4 MB (38.16%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        subarrays = 0 
+        currSum = 0
+        prefixSums = {0:1}
+        for n in nums:
+            currSum += n
+            diff = currSum - k
+            subarrays += prefixSums.get(diff, 0)
+            prefixSums[currSum] = 1 + prefixSums.get(currSum, 0)
+        return subarrays
