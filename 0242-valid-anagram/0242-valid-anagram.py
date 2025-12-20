4f02a4fdf067ae791ee8274f316223c523095e0e
Fitsum Azerefegne <mininifinds@gmail.com>
2025-12-20 21:08:03 +0300

    Time: 19 ms (14.84%), Space: 18 MB (47.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
-        hash_map = defaultdict(int)
-        
-        for letter in s:
-            hash_map[letter] += 1
-        
-        for letter in t:
-            hash_map[letter] -= 1
-            if hash_map[letter] < 0:
-                return False 
-        return True   
+        if sorted(s) == sorted(t):
+            return True
+        return False
