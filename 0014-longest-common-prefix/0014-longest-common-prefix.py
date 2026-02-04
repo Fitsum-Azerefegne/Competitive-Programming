31eb40192b2aa737c3b2a41f52af34b1a6a87228
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-04 10:51:03 +0300

    Time: 3 ms (26.17%), Space: 19.4 MB (17.42%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        first = strs[0]
+        for i in range(len(first)):
+            for s in strs[1:]:
+                if i >= len(s) or first[i] != s[i]:
+                    return res
+            res += first[i]
+        return res
+
+
+
+
+        
