13fa76fc91fd0b7e24ac26eee0532017d5470374
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-08 22:08:39 +0300

    Time: 149 ms (12.14%), Space: 19.8 MB (41.14%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,21 +1,9 @@
+from collections import Counter
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        chars_hash = defaultdict(int)
-        output = 0
-        for ch in chars:
-            chars_hash[ch] += 1 
+        chars_count = Counter(chars)
+        ans = 0
         for word in words:
-            hash_map = defaultdict(int)
-            valid = True
-            for char in word:
-                hash_map[char] += 1
-            for char in word: 
-                if char not in chars_hash or hash_map[char] > chars_hash[char]:
-                    valid = False
-                    break
-            if valid:
-                output += len(word)
-        return output
-            
-
-        
+            if not (Counter(word) - chars_count):
+                ans += len(word)
+        return ans
