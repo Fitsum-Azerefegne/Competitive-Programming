8016be7c6be7aa47cd737733c0d681db2a759762
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-09 11:36:05 +0300

    Time: 10 ms (27.97%), Space: 24.3 MB (18.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = Counter(nums).most_common(k)
 
-        res = []
-        for first, second in count:
-            res.append(first)
-        return res
-        
+        frequency = defaultdict(int)
+        for num in nums:
+            frequency[num] += 1
+
+        n = len(nums)
+        buckets = [[] for _ in range(n + 1)]
+
+        for num,freq in frequency.items():
+            buckets[freq].append(num)
+
+        result = []
+        for freq in range(n, 0, -1):  
+            for num in buckets[freq]:
+                result.append(num)
+                if len(result) == k:  
+                    return result
