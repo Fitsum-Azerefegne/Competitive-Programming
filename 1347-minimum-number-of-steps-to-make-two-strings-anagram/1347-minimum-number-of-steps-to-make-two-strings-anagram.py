1b7fd592bec4a9fc21ad4884fcd61f14578bf035
Fitsum Azerefegne <mininifinds@gmail.com>
2026-02-09 21:59:00 +0300

    Time: 55 ms (95.93%), Space: 19.9 MB (36.43%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,13 +1,13 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        hash_map = defaultdict(int)
+        sCounter = Counter(s)
+        tCounter = Counter(t)
         steps = 0
-        for letter in s:
-            hash_map[letter] += 1
-        for letter in t:
-            hash_map[letter] -= 1
-            if hash_map[letter] < 0:
-                steps += 1
-                hash_map[letter] += 1
+        for letter in sCounter:
+            if sCounter[letter] > tCounter[letter]:
+                steps += sCounter[letter] - tCounter[letter]
         return steps
-        
+
+
+
+
